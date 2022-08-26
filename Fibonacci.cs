3cb90565c5252a169db55b32facfe8924e1c3ced
nalpery <nalpery@users.noreply.github.com>
2022-08-26 06:54:57 +0300

    Update Fibonacci.cs

--- Fibonacci.cs
+++ Fibonacci.cs
@@ -3,11 +3,14 @@ int elementIndex = Convert.ToInt32(Console.ReadLine());
 int[] fibo=new int[elementIndex];
 fibo[0] = 1;
 fibo[1] = 1;
-for (int i = 2; i>=0; i++)
+if (elementIndex > 2) 
 {
-    
-    if (fibo[i] <= elementIndex) { fibo[i] = fibo[i-2] + fibo[i-1]; }
-    else { break; }
+    for (int i = 2; i >= 0; i++)
+    {
+
+        if (fibo[i] <= elementIndex) { fibo[i] = fibo[i - 2] + fibo[i - 1]; }
+        else { break; }
+    }
 }
 foreach (int i in fibo) {  Console.WriteLine(i); } 
 Console.ReadLine();
